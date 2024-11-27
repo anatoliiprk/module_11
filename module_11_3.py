8600473bae1f9489df565bea3ace47bbb8a413a3
anatoliiprk <anatoliiprk@gmail.com>
2024-11-27 22:20:00 +0700

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -9,7 +9,8 @@ def introspection_info(obj):
     attribute_list = []
     method_list = []
     for attr_name in dir(obj):
-        if callable(attr_name):
+        attr = getattr(obj, attr_name)
+        if callable(attr):
             method_list.append(attr_name)
         else:
             attribute_list.append(attr_name)
